269815e33aace23d1eecaab90a5bdadd09c9ab93
freitaslivia <xliviafreitasx@gmail.com>
2024-02-21 09:56:36 -0300

    projeto criado

---
+++ app/src/main/java/com/example/interfacegrafica/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.interfacegrafica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var name = findViewById<EditText>(R.id.txtNome)
+        var buttonText = findViewById<Button>(R.id.buttonTxt)
+
+        buttonText.setOnClickListener {
+            val msg = "Olá, ${name.text}"
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        }
+    }
+}
